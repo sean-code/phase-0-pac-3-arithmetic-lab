1d423f778f24f1cc1d4addfa25f4ac948673c3ec
sean-code <john.nganga@student.moringaschool.com>
2022-08-04 09:47:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add (){
+    console.log(add);
+} 
+add ();
+function subtract (){
+    console.log(subtract);
+} 
+subtract();
+
+function multiply (){
+    console.log(multiply);
+} 
+multiply ();
+
+function divide (){
+    console.log(divide);
+} 
+divide ();
+
+function add (a , b){
+    return a + b;
+}
+add (a , b);
+
+function subtract (a , b){
+    return a - b;
+}
+subtract (a , b);
+
+function multiply (a , b){
+    return a * b;
+}
+multiply (a , b);
+
+function divide (a , b){
+    return a / b;
+}
+divide (a , b);
+
+function increment(n) {
+    return (++n);
+  }
+
+  function decrement(n) {
+    return(--n);
+  }
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+
+  function preserveDecimal(n){
+    return parseFloat(n);
+  }
+
+
+
